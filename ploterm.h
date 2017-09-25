676e73e2c93a415b2b3c823f122e3678adfd3e53
Arnau Ramisa <arnauramisa@gmail.com>
2017-09-25 13:31:21 +0200

    h

---
+++ ploterm.h
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <cstdio>
+
+#include <sstream>
+#include <iomanip>
+
+std::string ascii_plot_simple_wrap(std::vector<float> &data, int W, int H);
